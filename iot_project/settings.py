78a050a74f5ef6ae4ae11a691a39f4f4c86a4def
Rovic Matthew Almonia <160310360+RovicAlmonia@users.noreply.github.com>
2025-05-16 14:42:15 +0800

    Update settings.py

--- iot_project/settings.py
+++ iot_project/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-_fdw@4snbf_nnq6ut)6cb&65b@)4p(ylr!k$10@-t46re-c8tl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.7', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
